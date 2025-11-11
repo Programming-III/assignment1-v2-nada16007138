edb1fb84cf5bd6a0c4b20e15dcaadff2e55506f6
nada16007138 <gadnada253@gmail.com>
2025-11-11 09:58:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,11 +20,6 @@ cout << "name of person:" << name << "id:" << id << endl;
 };
 
 
-
-
-
-
-
 // ==================== Student Class Implementation ====================
 
 class Student: public Person {
@@ -40,12 +35,20 @@ cout << "the yearlevel is" << yearLevel << "the major of the student" << major <
 
 
 // ==================== Instructor Class Implementation ====================
-public::
-Person (string depart, int expY){
-department=depart;
-experienceYears=expY
-void display ()
+class Instructor : public Person {
+private:
+    string department;
+    int experienceYears;
 
+public:
+    Instructor() : Person(), department(" "), experienceYears(0) {}
+    Instructor(string n, int i, string d, int e)
+        : Person(n, i), department(d), experienceYears(e) {}
+
+    void display() override {
+        cout << "Instructor Name: " << name << ",
+return 0; 
+}
 
 
 
@@ -75,7 +78,26 @@ cout << "the max num of students is " << maxStudents << "and the current student
 
 // ==================== Main Function ====================
 int main() {
-   
     
-    return 0;
-}
+    Instructor instructor("nada", 1001, "Computer Science", 10);
+    instructor.display();
+
+    // Create some students
+    Student s1("Nada", 2001, 2, "Software Engineering");
+    Student s2("Ali", 2002, 3, "Information Systems");
+
+    s1.display();
+    s2.display();
+
+    // Create a course and add students
+    Course course("CS101", "Introduction to Programming", 3);
+    course.addStudent(s1);
+    course.addStudent(s2);
+
+    // Display course info
+    course.displayCourseInfo();
+
+
+
+
+
